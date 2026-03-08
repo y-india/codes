5bc5eae4d0e0551df15ab33f93652a267284ee69
Yuvraj IND <y.india.main@gmail.com>
2026-03-08 12:44:18 +0530

    updates

--- keyboard_library/main.py
+++ keyboard_library/main.py
@@ -1,67 +1,68 @@
 import keyboard
 import time
 
-keyboard.write("Hello")
+# keyboard.write("Hello")
 
 
-# shift_check = keyboard.is_pressed("shift")  # Returns True if the Shift key is currently being pressed
+# # shift_check = keyboard.is_pressed("shift")  # Returns True if the Shift key is currently being pressed
 
-# keyboard.press("a")  # Simulates pressing the "a" key down
+# # keyboard.press("a")  # Simulates pressing the "a" key down
 
-# keyboard.release("a")  # Simulates releasing the "a" key
+# # keyboard.release("a")  # Simulates releasing the "a" key
 
-# print(shift_check)  # Output: True or False depending on whether Shift is pressed
+# # print(shift_check)  # Output: True or False depending on whether Shift is pressed
 
-"""
-Helloa
-Helloa
+# """
+# Helloa
+# Helloa
 
-Hello
-Helloaṁ
-
-
-My name is 
-
-"""
+# Hello
+# Helloaṁ
 
 
-"""
-keyboard.add_hotkey("alt+h+e", lambda: keyboard.write("Hello"))
-keyboard.wait()
-"""
+# My name is 
+
+# """
 
 
-keyboard.add_hotkey("ctrl+alt+m", lambda: keyboard.write("My name is "))
+# """
+# keyboard.add_hotkey("alt+h+e", lambda: keyboard.write("Hello"))
+# keyboard.wait()
+# """
 
 
-
-
-
-
-def write_name():
-    time.sleep(0.05)
-    keyboard.write("Rana Ji".upper())
-
-keyboard.add_hotkey("alt+r", write_name)
-
-keyboard.wait()
-
-
-"""
-Hello
-ṁMy name is 
-
-Hello
-ṁMy name is 
-NA JI
-na Ji
-Hello
-RANA JI
-
-
-
-"""
+# keyboard.add_hotkey("ctrl+alt+m", lambda: keyboard.write("My name is "))
 
 
 
 
+
+
+# def write_name():
+#     time.sleep(0.05)
+#     keyboard.write("Rana Ji".upper())
+
+# keyboard.add_hotkey("alt+r", write_name)
+
+# keyboard.wait()
+
+
+# """
+# Hello
+# ṁMy name is 
+
+# Hello
+# ṁMy name is 
+# NA JI
+# na Ji
+# Hello
+# RANA JI
+
+
+
+# """
+
+
+
+
+print("Hello")
